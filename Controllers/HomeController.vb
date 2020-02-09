6e876f9d56c3f6f531664a82014c50c900a76cb7
Amol Bhawari <amol.bhawari@gmail.com>
2020-02-09 17:30:58 +0530

    index added

---
+++ Controllers/HomeController.vb
@@ -0,0 +1,19 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Welcome") = "Your application description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("This is production server") = "Your contact page."
+
+        Return View()
+    End Function
+End Class
